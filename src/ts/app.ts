1ef9dd17f82f8e6cfff3721729c5a7626d747b4d
razon <shizonera@gmail.com>
2018-01-01 16:36:01 +0900

    implement actions

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,27 +2,63 @@
 
 import _ from 'lodash'
 import { app } from 'hyperapp'
-import { div, ol, li } from '@hyperapp/html'
+import { div, h1, ol, li, button } from '@hyperapp/html'
 
 const state = {
-  members: [
-    {userName: 'UserName1', displayName: 'DisplayName1'},
-    {userName: 'UserName2', displayName: 'DisplayName2'},
-    {userName: 'UserName3', displayName: 'DisplayName3'},
-    {userName: 'UserName4', displayName: 'DisplayName4'}
-  ]
+  members: []
 }
 
 const actions = {
-  down: (value: any) => (state: any) => ({ count: state.count - value }),
-  up: (value: any) => (state: any) => ({ count: state.count + value })
+  shuffle: () => (state: any) => ({members: _.shuffle(state.members)}),
+  dragover: (e: any) => (state: any) => dragOverHandler(e),
+  drop: (e: any) => (state: any, actions: any) => dropHandler(e, state, actions),
+  update: () => (state: any) => ({menbers: state.members})
+}
+
+function dragOverHandler(e: any) {
+  e.preventDefault();
+}
+
+const dropHandler = function(e: any, state: any, actions: any) {
+  e.stopPropagation()
+  e.preventDefault()
+  const files = e.dataTransfer.files
+  _.each(files, (file: any) => {
+    const reader = new FileReader()
+    reader.onload = () => {
+      state.members = _.chain(reader.result.split('\n'))
+      .tail()
+      .filter((line: string) => !_.isEmpty(line))
+      .filter((line: string) => line.split(',')[4] !== '参加キャンセル')
+      .map((line: string) => {
+        const row = line.split(',')
+        return {userName: row[1], displayName: row[2]}
+      }).value()
+      actions.update()
+    }
+    reader.readAsText(file, 'shift-jis')
+  })
 }
 
 const view = (state: any, actions: any) =>
-  div([
-    ol(_.map(state.members, (member: any) => 
-      li(member.displayName + '(' + member.userName + ')')
-    ))
+  div({class: 'app'}, [
+    div([
+      h1('connpassのCSVを読み込んで並べ替えるやつ'),
+      div({
+        class: 'droparea',
+        ondragover: actions.dragover,
+        ondrop: actions.drop
+      }, 'connpassのCSVファイルをドロップ'),
+      div({}, [button({
+        class: 'pure-button pure-button-primary',
+        onclick: actions.shuffle
+      }, '順番を並べ替える')]),
+      ol({
+        class: 'members'
+      }, _.map(state.members, (member: any) => 
+        li(member.displayName + '(' + member.userName + ')')
+      ))
+    ])
   ])
 
 const main = app(state, actions, view, document.body)
